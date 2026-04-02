8a1efeef2e4723266496834ffcbb97c1b3717019
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-04-02 15:49:03 -0300

    Adicionado método para gerar uma palavra aleatória

--- TermoPerto.ConsoleApp/Program.cs
+++ TermoPerto.ConsoleApp/Program.cs
@@ -1,18 +1,55 @@
-﻿namespace Termo.ConsoleApp
+﻿using System.Security.Cryptography;
+
+namespace TermoPerto.ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
             ExibirCabecalho();
+
+            GerarPalavraAleatoria();
         }
 
         static void ExibirCabecalho()
         {
             Console.Clear();
             Console.WriteLine("================================");
-            Console.WriteLine("---------- TERMO ---------------");
+            Console.WriteLine("-------------TERMO--------------");
             Console.WriteLine("================================");
+        }
+
+        static string GerarPalavraAleatoria()
+        {
+            string[] palavras = [
+                "Amigo",
+                "Verde",
+                "Nuvem",
+                "Tigre",
+                "Vento",
+                "Chuva",
+                "Folha",
+                "Praia",
+                "Sabor",
+                "Falar",
+                "Rosto",
+                "Lente",
+                "Canto",
+                "Brisa",
+                "Ferro",
+                "Pequi",
+                "Nobre",
+                "Tocar",
+                "Firme",
+                "Casal"
+            ];
+
+            int indiceAleatorio = RandomNumberGenerator.GetInt32(palavras.Length);
+
+            string palavraAleatoria = palavras[indiceAleatorio];
+
+            return palavraAleatoria;
+
         }
     }
 }
--- TermoPerto.ConsoleApp/obj/Debug/net10.0/TermoPerto.ConsoleApp.AssemblyInfo.cs
+++ TermoPerto.ConsoleApp/obj/Debug/net10.0/TermoPerto.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TermoPerto.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+397260d055cbf408d797df9a1556a6a976c0468e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TermoPerto.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TermoPerto.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
