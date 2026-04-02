1e07cd887f60e7b7a5a96d4bcd555a21d8f78e24
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-04-02 16:57:31 -0300

    Adicionada validação de entrada para o chute do usuário

--- TermoPerto.ConsoleApp/Program.cs
+++ TermoPerto.ConsoleApp/Program.cs
@@ -10,20 +10,38 @@ namespace TermoPerto.ConsoleApp
 
             string palavraAleatoria = GerarPalavraAleatoria();
 
-            Console.Write("> ");
-            string? chute = Console.ReadLine()?.ToUpper();
+            while (true)
+            {
+                Console.Write("Digite o seu chute: ");
+                string? chute = Console.ReadLine()?.ToUpper();
+
+                if (string.IsNullOrWhiteSpace(chute))
+                {
+                    Console.WriteLine("=======================================================");
+                    Console.WriteLine("Digite uma palavra válida.");
+                    Console.WriteLine("=======================================================");
+                    continue;
+                }
+                else if (chute.Length != 5)
+                {
+                    Console.WriteLine("\n=====================================================");
+                    Console.WriteLine("É necessário digitar uma palavra de no máximo 5 letras.");
+                    Console.WriteLine("=======================================================");
+                }
+            }
         }
 
         static void ExibirCabecalho()
         {
             Console.Clear();
-            Console.WriteLine("===========================================");
-            Console.Write("----------- TERMO ");
+            Console.WriteLine("=======================================================");
+            Console.Write("----------------- TERMO ");
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.Write("[5 letras] ");
             Console.ResetColor();
-            Console.WriteLine("--------------");
-            Console.WriteLine("===========================================");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("=======================================================");
+
         }
 
         static string GerarPalavraAleatoria()
