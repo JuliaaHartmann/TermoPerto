2ecb36d91bb41ec435c1ae596ef043b1b293a931
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-04-03 14:49:09 -0300

    Melhorada a lógica de tentativas e feedback ao usuário, incluindo contagem de tentativas restantes e mensagens de validação.

--- TermoPerto.ConsoleApp/Program.cs
+++ TermoPerto.ConsoleApp/Program.cs
@@ -7,51 +7,69 @@ namespace TermoPerto.ConsoleApp
     {
         static void Main(string[] args)
         {
-            ExibirCabecalho();
-
-            string palavraAleatoria = GerarPalavraAleatoria();
-
             while (true)
             {
-                Console.Write("Digite o seu chute: ");
-                string? chute = Console.ReadLine()?.ToUpper();
+                ExibirCabecalho();
 
-                if (string.IsNullOrWhiteSpace(chute))
-                {
-                    Console.WriteLine("=======================================================");
-                    Console.WriteLine("Digite uma palavra válida.");
-                    Console.WriteLine("=======================================================");
-                    continue;
-                }
-                else if (chute.Length != 5)
-                {
-                    Console.WriteLine("\n=====================================================");
-                    Console.WriteLine("É necessário digitar uma palavra de no máximo 5 letras.");
-                    Console.WriteLine("=======================================================");
-                }
+                string palavraAleatoria = GerarPalavraAleatoria();
+                Console.WriteLine(palavraAleatoria);
+                int tentativasMaximas = 5;
+                int tentativa;
 
-                if (chute == palavraAleatoria)
+                for (tentativa = tentativasMaximas; tentativa >= 1; tentativa--)
                 {
-                    Console.BackgroundColor = ConsoleColor.DarkGreen;
-                    Console.WriteLine(chute);
-                    Console.ResetColor();
-                    Console.WriteLine($"\nVocê acertou a palavra! Parabéns!");
-                    //break;
-                }
-                
-                for (int letraChute = 0; letraChute < chute.Length; letraChute++)
-                {
-                    Console.Write("< ");
-                    if (chute[letraChute] == palavraAleatoria[letraChute])
+                    Console.WriteLine();
+                    Console.Write("> ");
+                    string? chute = Console.ReadLine()?.ToUpper();
+
+                    if (string.IsNullOrWhiteSpace(chute))
+                    {
+                        Console.WriteLine("=======================================================");
+                        Console.WriteLine("Digite uma palavra válida.");
+                        Console.WriteLine("=======================================================");
+                        tentativa++;
+                        continue;
+                    }
+                    else if (chute.Length != 5)
+                    {
+                        Console.WriteLine("\n=====================================================");
+                        Console.WriteLine("É necessário digitar uma palavra de no máximo 5 letras.");
+                        Console.WriteLine("=======================================================");
+                        tentativa++;
+                    }
+
+                    if (chute == palavraAleatoria)
+                    {
                         Console.BackgroundColor = ConsoleColor.DarkGreen;
-                    else if (palavraAleatoria.Contains(chute))
-                        Console.BackgroundColor = ConsoleColor.DarkYellow;
-                    else
-                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.Write(chute);
+                        Console.ResetColor();
+                        Console.WriteLine($"\nVocê acertou a palavra! Parabéns!");
+                        Console.ReadLine();
+                        break;
+                    }
 
-                    Console.Write(letraChute);
+                    Console.Write("< ");
+
+                    for (int letraChute = 0; letraChute < chute.Length; letraChute++)
+                    {
+                        if (chute[letraChute] == palavraAleatoria[letraChute])
+                            Console.BackgroundColor = ConsoleColor.DarkGreen;
+                        else if (palavraAleatoria.Contains(chute[letraChute]))
+                            Console.BackgroundColor = ConsoleColor.DarkYellow;
+                        else
+                            Console.BackgroundColor = ConsoleColor.DarkRed;
+
+                        Console.Write(chute[letraChute]);
+                        Console.ResetColor();
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write($"  [{tentativa}] tentativas restantes...");
                     Console.ResetColor();
-                }        
+                }
+
+                if (tentativa == 0)
+                    Console.WriteLine("\nVocê atingiu o número máximo de tentativas...");
 
                 if (!DesejaContinuar())
                     break;
@@ -104,7 +122,7 @@ namespace TermoPerto.ConsoleApp
 
         static bool DesejaContinuar()
         {
-            Console.Write("Deseja continuar? (S/N): ");
+            Console.Write("\nDeseja jogar novamente? (S/N): ");
             string? opcaoContinuar = Console.ReadLine();
 
             if (opcaoContinuar?.ToUpper() != "S")
